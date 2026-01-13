c07beb8f5c8bd8893f63ad13a7ecd77e6599d567
SaiVardhanKatta <saivardhangoud.k@gmail.com>
2026-01-13 02:01:48 +0000

    Sync LeetCode submission Runtime - 4 ms (99.67%), Memory - 47.9 MB (16.16%)

---
+++ leetcode/0680-valid-palindrome-ii/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length()-1;
+
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r)){
+                return isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    private static boolean isPalindrome(String str, int l, int r){
+        while(l < r){
+            if(str.charAt(l) != str.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
